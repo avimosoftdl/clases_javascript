491e090f4f15591dd62753bb8636c1809d326b25
Adrián Avilés <adrian.aviles@avimosoft.com>
2015-05-14 20:59:11 -0500

    subiendo archivo script.js modificado

---
+++ script.js
@@ -0,0 +1,17 @@
+var isVerified = true;
+console.log(isVerified);
+
+var address = "Av. Juárez No. 101 L 317";
+console.log(address);
+
+var age = 26;
+console.log(age);
+
+var price = 10.60;
+console.log(price);
+
+var name;
+console.log(name);
+
+var nullValue = null;
+console.log(nullValue);
